ca127ec539351180074c40335a49f4ce2afd7f81
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2024-10-22 13:59:12 +0530

    new

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Adding viewport meta for responsiveness */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <body className="antialiased bg-white dark:bg-gray-900">
         <Main />
         <NextScript />
       </body>
